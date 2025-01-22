7c3eae47b0605e35a17c18bde5362f4ed60772a3
Ash Sewnandan <9801346+asewnandan@users.noreply.github.com>
2025-01-22 17:01:23 +0100

    Added Npuls Logo
    Thanks to @Tomeriko96

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(layout="wide")
 
 def main():
-    st.markdown("# CEDA")
+   with st.sidebar:
+       col1, col2, col3 = st.columns([1,2,1])
+       with col2:
+           image = Image.open('src/assets/Npuls_logo.png')
+           st.image(image, use_container_width=True)
+   st.markdown("# Seh")
 
 if __name__ == "__main__":
-    main()
+   main()
