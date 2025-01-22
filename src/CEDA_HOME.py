63973c4bc2aa5f7920d650325a347899f2d63a55
tin900 <t.iwan@vu.nl>
2025-01-22 17:55:50 +0100

    update: color scheme

--- src/CEDA_HOME.py
+++ src/CEDA_HOME.py
@@ -8,7 +8,14 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+# st.sidebar.success("Select a demo above.")
 
 def main():
    with st.sidebar:
@@ -16,7 +23,24 @@ def main():
        with col2:
            image = Image.open('src/assets/Npuls_logo.png')
            st.image(image, use_container_width=True)
-   st.markdown("# Home")
 
 if __name__ == "__main__":
    main()
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
