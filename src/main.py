0c94a4359a62611bcb134ad4f2ad188396d3cf32
Ash Sewnandan <9801346+asewnandan@users.noreply.github.com>
2025-01-29 01:22:44 +0100

    Update main.py
    Test for screen_scanner()

--- src/main.py
+++ src/main.py
@@ -1,20 +1,31 @@
 import streamlit as st
-from config.screen_scanner import *
+from config.screen_scanner import get_screens
+
+st.set_page_config(page_title="Developer Preview", page_icon=":material/edit:")
 
 ###
 # Import Screens
 ###
 pages =  get_screens()
-test = st.Page("screens/Home.py", title="Bug reports", icon=":material/bug_report:")
 
-# Use list comprehension to create Page objects in st.navigation
+test = st.Page(
+    pages[0]['path'],
+    title=pages[0]['title'],
+    icon=pages[0]['icon']
+)
+
+best = st.Page(
+    pages[2]['path'],
+    title=pages[2]['title'],
+    icon=pages[2]['icon']
+)
 # pg = st.navigation([st.Page(path) for path in page_paths])
-pg = st.navigation([test])
+pg = st.navigation([test, best])
+
 
 ###
 # Run Navigation / APP
 ###
-st.set_page_config(page_title="Developer Preview", page_icon=":material/edit:")
 pg.run()
 
 
