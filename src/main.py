7fa61fbc8a80fe43151dedbd1276ca5c5a23f8e3
Ash Sewnandan <9801346+asewnandan@users.noreply.github.com>
2025-01-29 19:33:08 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,33 +1,28 @@
 import streamlit as st
 from config.screen_scanner import get_screens
 
+###
+# Set Page Config
+###
 st.set_page_config(page_title="Developer Preview", page_icon=":material/edit:")
 
 ###
-# Import Screens
+# Initialize Navigation
 ###
 pages =  get_screens()
 
-test = st.Page(
-    pages[0]['path'],
-    title=pages[0]['title'],
-    icon=pages[0]['icon']
-)
-
-best = st.Page(
-    pages[2]['path'],
-    title=pages[2]['title'],
-    icon=pages[2]['icon']
-)
-# pg = st.navigation([st.Page(path) for path in page_paths])
-pg = st.navigation([test, best])
-
+pg = st.navigation([
+    st.Page(
+        page['path'],
+        title=page['title'],
+        icon=page['icon']
+    ) for page in pages
+])
 
 ###
-# Run Navigation / APP
+# Run Page
 ###
 pg.run()
-
 
 ###
 # TODO
