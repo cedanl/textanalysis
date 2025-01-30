8188e96687a69a8b14d6decd4032881f9f1d1789
Ash Sewnandan <9801346+asewnandan@users.noreply.github.com>
2025-01-30 14:33:25 +0100

    Changed utils to logic

--- src/screens/Home/Home.py
+++ src/screens/Home/Home.py
@@ -4,10 +4,28 @@ import streamlit as st
 title = "Home"
 icon = ":material/home:"
 
-st.title("Welcome to the home page!")
+st.title("Welcome to the home page Corneel!")
 
 # Display or edit DataFrame if available
 if st.session_state.df is not None:
     st.session_state.df = st.data_editor(st.session_state.df)
 else:
     st.write("No File available. Please upload a file in the sidebar.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
