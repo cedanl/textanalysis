9b4cb22c6ab0ec2f0acda8182cf1615d5ed5eb5d
Ash Sewnandan <9801346+asewnandan@users.noreply.github.com>
2025-01-30 12:55:39 +0100

    Updated Comment Style main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,20 @@
 import streamlit as st
 from config.screen_scanner import get_screens, group_pages_by_subdirectory
 from config.file_handler import file_handler
-###
+# ---------------------------------------
 # TODO
-###
+# ---------------------------------------
 # [] Veranderen main.py -> CEDA.py | CEDA_run.py?
 # Add directory ranking/page ranking per file for sorting?
 
-
-###
-# General Page Configuration
-###
+# ---------------------------------------
+# GENERAL PAGE CONFIGURATION
+# ---------------------------------------
 st.set_page_config(page_title="CEDA Preview", page_icon=":material/edit:")
 
-###
-# Sidebar Configuration
-###
+# ---------------------------------------
+# SIDEBAR CONFIGURATION
+# ---------------------------------------
 # Get all pages | Group by subdirectory | Create Streamlit Objects
 pages = get_screens()
 grouped_pages = group_pages_by_subdirectory(pages)
@@ -27,8 +26,8 @@ pg = st.navigation(grouped_pages)
 if __name__ == '__main__':
     file_handler()
 
-###
-# Run the Streamlit Navigation
-###
+# ---------------------------------------
+# RUN STREAMLIT | PAGES | NAVIGATION
+# ---------------------------------------
 pg.run()
 
