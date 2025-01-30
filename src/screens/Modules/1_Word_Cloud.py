d62d3f1a5c3a61383199d2697010ef3239a1695f
Tomer <t.iwan@vu.nl>
2025-01-30 17:04:36 +0100

    feat: get working wordcloud

--- src/screens/Modules/1_Word_Cloud.py
+++ src/screens/Modules/1_Word_Cloud.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from logic.Word_Cloud import generate_wordcloud
 
 # ---------------------------------------
 # PAGE CONFIGURATION
@@ -9,3 +10,11 @@ icon = ":material/cloud_queue:"
 # PAGE ELEMENTS
 # ---------------------------------------
 st.title("Word cloud page")
+
+# Display or edit DataFrame if available
+if st.session_state.df is not None:
+    st.session_state.df = st.data_editor(st.session_state.df, key="main_data_editor")
+
+    generate_wordcloud()
+else:
+    st.write("No DataFrame available. Please upload a file.")
